3332b4d66c112a3132a583cdc041cfe1feaf5455
ANGGIT NURHIDAYAH <anggitnurhidayah@gmail.com>
2023-12-31 11:20:37 +0700

    fix-seeder-str-db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
