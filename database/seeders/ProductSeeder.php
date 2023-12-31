a8a35bad69fd1cbef2dcc786f5cb2077acc50dab
ANGGIT NURHIDAYAH <anggitnurhidayah@gmail.com>
2023-12-31 11:22:07 +0700

    fix-sseder-str-db

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
