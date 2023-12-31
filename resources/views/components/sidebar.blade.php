9f4546c72fcce9796b10cb44d57b345b14ed5cf7
ANGGIT NURHIDAYAH <anggitnurhidayah@gmail.com>
2023-12-31 13:33:22 +0700

    update Name

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,13 +1,13 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Selamat Datang</a>
+            <a href="www.xclude.id">ANGGIT HIDAYAH</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">Xcl</a>
+            <a href="www.xclude.id">Xcl</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard Xclude</li>
+            <li class="menu-header">Green House Coffee & Eatery</li>
             <li class="nav-item dropdown">
                 <a href="#"
                     class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
